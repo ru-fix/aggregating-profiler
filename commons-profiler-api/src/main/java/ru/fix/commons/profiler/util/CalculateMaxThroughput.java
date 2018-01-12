d51d33cb631c690c073bc9ab651380080295beb0
Gleb Beliaev <gbeliaev@fix.ru>
2018-01-12 17:05:25 +0300

    add calculate Throughput

--- commons-profiler-api/src/main/java/ru/fix/commons/profiler/util/CalculateMaxThroughput.java
+++ commons-profiler-api/src/main/java/ru/fix/commons/profiler/util/CalculateMaxThroughput.java
@@ -1,5 +1,8 @@
 package ru.fix.commons.profiler.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -7,6 +10,8 @@ import java.util.concurrent.atomic.AtomicLong;
  * Created 11.01.18.
  */
 public class CalculateMaxThroughput {
+    private static final Logger log = LoggerFactory.getLogger(CalculateMaxThroughput.class);
+
     private final AtomicLong maxCallCountPerSecond = new AtomicLong();
     private final AtomicLong callCount = new AtomicLong();
     private final AtomicLong timeBeginningOfSecond = new AtomicLong();
@@ -19,16 +24,19 @@ public class CalculateMaxThroughput {
 
     public void call(long eventCount) {
         long time = timeBeginningOfSecond.get();
-        long count = callCount.addAndGet(eventCount);
+        long count = callCount.getAndAdd(eventCount);
         long now = currentTimeMillis();
 
-        if (time + ONE_SECOND_MS < now) {
+        log.trace("begin time {}, count {}, now {}", time, count, now);
+
+        if (time + ONE_SECOND_MS <= now) {
             if (timeBeginningOfSecond.compareAndSet(time, now)) {
                 callCount.addAndGet(-count);
 
                 boolean doWhile;
                 do {
                     long max = maxCallCountPerSecond.get();
+                    log.trace("update max, count {}, max {}", count, max);
                     doWhile = count > max && !maxCallCountPerSecond.compareAndSet(max, count);
                 } while (doWhile);
             }
@@ -40,9 +48,10 @@ public class CalculateMaxThroughput {
     }
 
     public long getMaxAndReset() {
-        /**
-         * update current max in case there was no calls involved before building report
+        /*
+          update current max in case there was no calls involved before building report
          */
+        log.trace("reset");
         call(0);
         return maxCallCountPerSecond.getAndSet(0);
     }
--- commons-profiler-api/src/test/java/ru/fix/commons/profiler/util/CalculateMaxThroughputTest.java
+++ commons-profiler-api/src/test/java/ru/fix/commons/profiler/util/CalculateMaxThroughputTest.java
@@ -25,7 +25,7 @@ public class CalculateMaxThroughputTest {
         c.call();
         assertEquals(2, c.getMaxAndReset());
 
-        when(c.currentTimeMillis()).thenReturn(3000L, 4000L, 4100L, 4200L);
+        when(c.currentTimeMillis()).thenReturn(3000L, 4000L, 4100L, 4200L, 5000L);
         c.call();
         c.call();
         c.call();
@@ -33,5 +33,10 @@ public class CalculateMaxThroughputTest {
         assertEquals(3, c.getMaxAndReset());
 
         assertEquals(0, c.getMaxAndReset());
+    }
+
+    @Test
+    public void name1() throws Exception {
+
     }
 }
