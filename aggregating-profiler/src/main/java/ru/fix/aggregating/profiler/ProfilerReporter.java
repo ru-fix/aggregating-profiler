c654b560f7f76db001246c134f4c86d3c866ba01
kasfandiyarov <kasfandiyarov@fix.ru>
2018-08-02 12:39:00 +0300

    Add proposal to fix api with regexp filtering

--- aggregating-profiler/src/main/java/ru/fix/aggregating/profiler/ProfilerReporter.java
+++ aggregating-profiler/src/main/java/ru/fix/aggregating/profiler/ProfilerReporter.java
@@ -1,14 +1,6 @@
 package ru.fix.aggregating.profiler;
 
-import java.util.List;
-import java.util.regex.Pattern;
-
 public interface ProfilerReporter extends AutoCloseable {
 
     ProfilerReport buildReportAndReset();
-
-    /**
-     * @return empty report in case of empty patterns
-     */
-    ProfilerReport buildReportAndReset(List<Pattern> patterns);
 }
