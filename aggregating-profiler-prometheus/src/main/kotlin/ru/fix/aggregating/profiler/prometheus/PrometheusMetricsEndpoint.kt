c06ee56c0ce75ebec84ef001a34447a59babed03
kasfandiyarov <VkyabOvtTzp1821>
2018-12-09 21:22:07 +0300

    Add prometheus endpoint.

---
+++ aggregating-profiler-prometheus/src/main/kotlin/ru/fix/aggregating/profiler/prometheus/PrometheusMetricsEndpoint.kt
@@ -0,0 +1,5 @@
+package ru.fix.aggregating.profiler.prometheus
+
+class PrometheusMetricsEndpoint{
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,5 @@
 rootProject.name = "aggregating-profiler"
 include("aggregating-profiler")
 include("aggregating-profiler-graphite")
+include("aggregating-profiler-prometheus")
 include("aggregating-profiler-jmh")
