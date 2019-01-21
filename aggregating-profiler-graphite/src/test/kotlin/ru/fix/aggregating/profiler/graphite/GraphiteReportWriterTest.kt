fae36bb728afc5ef173b703e38929ebb38252562
kasfandiyarov <kasfandiyarov@gmail.com>
2019-01-21 10:49:57 +0300

    Fix metrics naming. Provide identity string.

--- aggregating-profiler-graphite/src/test/kotlin/ru/fix/aggregating/profiler/graphite/GraphiteReportWriterTest.kt
+++ aggregating-profiler-graphite/src/test/kotlin/ru/fix/aggregating/profiler/graphite/GraphiteReportWriterTest.kt
@@ -3,14 +3,18 @@ package ru.fix.aggregating.profiler.graphite
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers
 import org.junit.Assert
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper
 import ru.fix.aggregating.profiler.AggregatingProfiler
 import ru.fix.aggregating.profiler.Identity
 import ru.fix.aggregating.profiler.graphite.client.GraphiteEntity
+import ru.fix.aggregating.profiler.graphite.client.GraphiteSettings
 import ru.fix.aggregating.profiler.graphite.client.GraphiteWriter
+import ru.fix.aggregating.profiler.graphite.client.ProtocolType
 
 @ExtendWith(MockKExtension::class)
 class GraphiteReportWriterTest {
@@ -61,5 +65,18 @@ class GraphiteReportWriterTest {
         assertEntryExist { name == "indicator2.type.hard.indicatorMin" && value =="43" }
         assertEntryExist { name == "call1.stopSum" && value == "1" }
         assertEntryExist { name == "call2.type.soft.stopSum" && value == "1" }
+    }
+
+    @Test
+    fun `serialize settings`(){
+        GraphiteProfilerReporterSettings(
+                GraphiteSettings(port = 2003, host = "localhost", batchSize = 500, protocol = ProtocolType.TCP),
+                SelectiveRateProfilingConfig(defaultTimeout = 60_000L, reportingSchedule = mutableMapOf(80_000 to listOf(".*HTTP.*", ".*WEB.*")))
+        ).let {
+            ObjectMapper().writeValueAsString(it)
+        }.let {
+            assertThat(it, Matchers.containsString("localhost") )
+            assertThat(it, Matchers.containsString("HTTP") )
+        }
     }
 }
