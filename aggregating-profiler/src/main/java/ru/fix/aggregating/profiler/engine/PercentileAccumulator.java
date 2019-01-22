7d278391d589574360ae1146005ced52b000c0db
kasfandiyarov <kasfandiyarov@gmail.com>
2019-01-22 15:01:39 +0300

    Add tag to reporter

--- aggregating-profiler/src/main/java/ru/fix/aggregating/profiler/engine/PercentileAccumulator.java
+++ aggregating-profiler/src/main/java/ru/fix/aggregating/profiler/engine/PercentileAccumulator.java
@@ -36,6 +36,10 @@ public class PercentileAccumulator {
         return buckets;
     }
 
+    /**
+     * @param currentMaximum maximum value of the metric that Reporter saw during reporting period
+     *                       minor optimization so PercentileAccumulator do not need to track this value by itself
+     */
     public Map<Integer, Long> buildAndReset(long currentMaximum) {
 
         TreeMap<Long, Long> counts = new TreeMap<>();
@@ -60,6 +64,31 @@ public class PercentileAccumulator {
 
         Map<Integer, Long> report = new HashMap<>();
 
+        // currentCounts maintain total count of measurements
+        // each bucket will increase currentCounts level
+        // if there are 200 measurement in total
+        // then 90 percentile is value of 180th measure
+        //      95 percentile is value of 190th measure
+        // suppose that our buckets holds:
+        // bucket[0] = 100
+        // bucket[1] = 40
+        // bucket[2] = 120
+        //
+        // the currentValue will be:
+        // level 0 is 100
+        // level 1 is 140
+        // level 2 is 260
+        //
+        // for level 0 and 1 currentValue is not enough to fill report
+        // for level 2 currentCounts becomes 260. It is bigger than both of percentiles: 180(for 90p) and 190(for 95p).
+        // We will set same 260th measure value for both percentiles in report.
+        // This will lead to a situation when 90 percentile is equal to 95 percentile.
+        // 90 percentile is bigger that real value. But due to inaccuracy we fill it at least with
+        // not accurate value that we have.
+        //
+        // Actual percentile value is less or equal to the one that we are reporting.
+        //
+        // TODO: add to readme
         for (Map.Entry<Long, Long> level : counts.entrySet()) {
             currentCounts += level.getValue();
 
