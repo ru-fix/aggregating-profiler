7ed2516f0f3f0e8f901060316a50c2dddb21bec7
kasfandiyarov <kasfandiyarov@gmail.com>
2020-08-05 19:00:27 +0300

    Add test for indidcator

--- aggregating-profiler/src/test/kotlin/ru/fix/aggregating/profiler/PrefixedProfilerTest.kt
+++ aggregating-profiler/src/test/kotlin/ru/fix/aggregating/profiler/PrefixedProfilerTest.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test
 class PrefixedProfilerTest {
 
     @Test
-    fun `prefixed profiler adds prefix to name and tags to identity`() {
+    fun `prefixed profiler adds prefix to name and tags to identity of a profiled call`() {
         val profiler = AggregatingProfiler()
         val prefixedProfiler = PrefixedProfiler(profiler, "prefix", mapOf("prefixTagName" to "prefixTagValue"))
 
@@ -26,4 +26,25 @@ class PrefixedProfilerTest {
         }
     }
 
+    @Test
+    fun `prefixed profiler adds prefix to name and tags to identity of an indicator`() {
+        val profiler = AggregatingProfiler()
+        val prefixedProfiler = PrefixedProfiler(profiler, "prefix", mapOf("prefixTagName" to "prefixTagValue"))
+
+        val reporter = profiler.createReporter()
+
+        prefixedProfiler.attachIndicator(Identity(
+                "temperature", "temperatureTagName", "temperatureTagValue")) {
+            42L
+        }
+
+        reporter.buildReportAndReset().indicators.entries.single().let { (identity, value) ->
+            identity.name.shouldBe("prefix.temperature")
+            identity.tags.shouldContainExactly(mapOf(
+                    "prefixTagName" to "prefixTagValue",
+                    "temperatureTagName" to "temperatureTagValue"
+            ))
+            value.shouldBe(42)
+        }
+    }
 }
